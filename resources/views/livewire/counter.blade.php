433dd9167ec66a8129f77c16c9eecf5c40ae24eb
Jakob Lau Nielsen <1234+frakke@users.noreply.github.com>
2021-02-02 21:45:37 +0100

    Tailwinding

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,7 +1,7 @@
 <div style="text-align: center">
-    <button wire:click="fastest1km" class="underline text-gray-900 dark:text-white bg-gray-200 p-6 sm:rounded-lg">PR 1km</button>
-    <button wire:click="fastest5km" class="underline text-gray-900 dark:text-white bg-gray-200 p-6 sm:rounded-lg">PR 5km</button>
-    <button wire:click="fastest10km" class="underline text-gray-900 dark:text-white bg-gray-200 p-6 sm:rounded-lg">PR 10km</button>
-    <button wire:click="fastest21km" class="underline text-gray-900 dark:text-white bg-gray-200 p-6 sm:rounded-lg">PR 21km</button>
+    <button wire:click="fastest1km" class="uppercase px-8 py-2 rounded-full bg-yellow-500 text-blue-50 max-w-max shadow-sm hover:shadow-lg">PR 1km</button>
+    <button wire:click="fastest5km" class="uppercase px-8 py-2 rounded-full bg-yellow-500 text-blue-50 max-w-max shadow-sm hover:shadow-lg">PR 5km</button>
+    <button wire:click="fastest10km" class="uppercase px-8 py-2 rounded-full bg-yellow-500 text-blue-50 max-w-max shadow-sm hover:shadow-lg">PR 10km</button>
+    <button wire:click="fastest21km" class="uppercase px-8 py-2 rounded-full bg-yellow-500 text-blue-50 max-w-max shadow-sm hover:shadow-lg">PR 21km</button>
     <h1>{{ $speed }}</h1>
 </div>
